0efbf87f25e0b05bfbd4181bffba5613b4f53569
Sergey Baikov <rayleonard9@gmail.com>
2020-09-28 15:33:12 +0300

    Servlets: Login, Filter, Reg, Main Page

---
+++ src/main/java/ru/job4j/presentation/ChangeSellOrderStatus.java
@@ -0,0 +1,6 @@
+package ru.job4j.presentation;
+
+import javax.servlet.http.HttpServlet;
+
+public class ChangeSellOrderStatus extends HttpServlet {
+}
---
+++ src/main/java/ru/job4j/presentation/DeleteSellOrder.java
@@ -0,0 +1,6 @@
+package ru.job4j.presentation;
+
+import javax.servlet.http.HttpServlet;
+
+public class DeleteSellOrder extends HttpServlet {
+}

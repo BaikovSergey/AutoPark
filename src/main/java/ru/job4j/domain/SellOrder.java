06ed39f2fb6c736520e2e3ee0cc956da8d4a2a07
Sergey Baikov <rayleonard9@gmail.com>
2020-09-28 11:55:35 +0300

    refactor

--- src/main/java/ru/job4j/domain/SellOrder.java
+++ src/main/java/ru/job4j/domain/SellOrder.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
-@Table(name = "auto_park_sell_order")
+@Table(name = "auto_park_sell_orders")
 public class SellOrder {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
